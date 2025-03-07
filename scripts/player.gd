8b4a75095d8b8b9392fab429da3987e94f51bc0f
Luiz Henrique <luizhsgassis.dev@gmail.com>
2025-03-06 21:23:23 -0300

    Aula_06_03
    Criação dos TileMaps, fazendo o mapa e configurando as colisões da fase.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 70.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -16,13 +16,13 @@ func _physics_process(delta: float) -> void:
 		else:
 			anim.play("idle")
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
