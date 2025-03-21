4c4ad3bb8309a6241d4d91ade1fad4e8e6989a51
Luiz Henrique <luizhsgassis.dev@gmail.com>
2025-03-20 21:35:53 -0300

    feat:
    Adicionado câmera como entidade para acompanhar o player, e adicionados áreas de morte e final de fase.

---
+++ scripts/camera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+var player: Node2D
+
+func _ready() -> void:
+	var players = get_tree().get_nodes_in_group("Player")
+	if players.size() > 0:
+		player = players[0]
+	else:
+		push_error("Não foi possível encontrar o player")
+
+func _process(delta: float) -> void:
+	if player:
+		position = lerp(position, player.position, 0.1)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,33 +2,35 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 70.0
-const JUMP_VELOCITY = -300.0
+const speed = 70.0
+const jumpVelocity = -300.0
 
-var jump_count = 0
-
+@export var maxJump = 2
+var jumpCount = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
+		jumpCount = 0
 		if velocity.x != 0:
 			anim.play("running")
 		else:
 			anim.play("idle")
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and jumpCount < maxJump:
+		velocity.y = jumpVelocity
+		jumpCount += 1
 		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	if velocity.x > 0:
 		anim.flip_h = false
@@ -36,3 +38,9 @@ func _physics_process(delta: float) -> void:
 		anim.flip_h = true
 
 	move_and_slide()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group('Death Zone'):
+		get_tree().reload_current_scene()
+	elif area.is_in_group('End Level'):
+		get_tree().change_scene_to_file("res://scenes/tropic.tscn")
